faa1c21dfe0daeff12fba127406b4826ea0aac0f
kevin <1910900661@qq.com>
2020-02-12 19:09:55 +0800

    init

---
+++ src/test/java/com/ruoyi/project/system/user/controller/UserControllerTest.java
@@ -0,0 +1,99 @@
+package com.ruoyi.project.system.user.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testUser() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testList() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExport() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testImportData() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testImportTemplate() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSave() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEdit() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditSave() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResetPwd() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResetPwdSave() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckLoginNameUnique() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckPhoneUnique() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckEmailUnique() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChangeStatus() {
+//		fail("Not yet implemented");
+	}
+
+}
